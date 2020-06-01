c6d16ce7e541a550d87e48bdfcb5a189d82e161f
Euan Medway <euanmedway86@gmail.com>
2020-06-01 17:17:52 +0100

    Created initial player spawn database insertion.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,1 +1,12 @@
 include("shared.lua")
+
+function GM:PlayerInitialSpawn(ply)
+
+    gloonz.database.connect()
+
+    local run = gloonz_database:query("INSERT INTO players VALUES(" .. ply:SteamID() .. ", " .. ply:Name() .. ", " .. 0 .. ", " .. 0 .. ", " .. 0 .. ")")
+    run.onSuccess = function(q, data) print("Query: " .. q .. " was successful") print(data) gloonz.database.disconnect() end
+    run.onError = function(q, e) print("An error happened with query: " .. q .. "\n".. e) gloonz.database.disconnect() end
+    run:start()
+
+end

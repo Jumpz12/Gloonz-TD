d4538db309b4780bf270dd78df63f5cc8fc0209c
Euan Medway <euanmedway86@gmail.com>
2020-06-02 17:35:14 +0100

    Made players spawn at their base if one exists.

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,35 @@
 include("shared.lua")
+
+function set_team() 
+ 
+    frame = vgui.Create( "DFrame" ) 
+    frame:SetPos( 100, ScrH() / 2 )
+    frame:SetSize( 200, 210 ) 
+    frame:SetTitle( "Change Team" )
+    frame:SetVisible( true ) 
+    frame:SetDraggable( false ) 
+    frame:ShowCloseButton( true ) 
+    frame:MakePopup() 
+    
+    team_1 = vgui.Create( "DButton", frame ) 
+    team_1:SetPos( 30, 30 )
+    team_1:SetSize( 100, 50 ) 
+    team_1:SetText( "Team 1" ) 
+    team_1.DoClick = function()
+    
+        RunConsoleCommand( "team_1" ) 
+    
+    end 
+    
+    team_2 = vgui.Create( "DButton", frame ) 
+    team_2:SetPos( 30, 85 )
+    team_2:SetSize( 100, 50 ) 
+    team_2:SetText( "Team 2" ) 
+    team_2.DoClick = function() 
+    
+        RunConsoleCommand( "team_2" ) 
+    
+    end 
+    
+end 
+concommand.Add( "team_menu", set_team ) 
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,5 +4,40 @@ function GM:PlayerInitialSpawn(ply)
 
     gloonz.database.insertToTable("players", ply:SteamID(), ply:Name(), 0, 0, 0)
     ply:SetTeam(1)
+    ply:Lock()
+    ply:ConCommand( "team_menu" )
 
 end
+
+function GM:PlayerSelectSpawn(ply)
+
+    local entity = ents.FindByClass("base_spawn")
+
+    for k, v in pairs(entity) do
+
+        if v.team == ply:Team() then
+
+            return v
+
+        end
+        
+    end
+
+end
+
+function team_1( ply ) 
+ 
+    ply:SetTeam( 2 )
+    ply:Spawn()
+
+end 
+
+function team_2( ply ) 
+
+    ply:SetTeam( 3 )
+    ply:Spawn()
+
+end 
+
+concommand.Add( "team_1", team_1 )
+concommand.Add( "team_2", team_2 )

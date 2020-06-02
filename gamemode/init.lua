0afe68da861a0341a53c37bf760b54d2c848078c
Euan Medway <euanmedway86@gmail.com>
2020-06-02 16:03:35 +0100

    Updated PlayerInitialSpawn to use new function.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,10 +3,6 @@ include("shared.lua")
 function GM:PlayerInitialSpawn(ply)
 
     gloonz.database.connect()
-
-    local run = gloonz_database:query("INSERT INTO players VALUES(" .. ply:SteamID() .. ", " .. ply:Name() .. ", " .. 0 .. ", " .. 0 .. ", " .. 0 .. ")")
-    run.onSuccess = function(q, data) print("Query: " .. q .. " was successful") print(data) gloonz.database.disconnect() end
-    run.onError = function(q, e) print("An error happened with query: " .. q .. "\n".. e) gloonz.database.disconnect() end
-    run:start()
+    gloonz.database.insertToTable("players", ply:SteamID(), ply:Name(), 0, 0, 0)
 
 end
